b137da6d0cdfe4afb9e81884e05757ee1fcd5565
BRCMOURA <brcmoura@gmail.com>
2024-10-04 20:49:13 -0300

    Add files via upload

---
+++ Exercício2.py
@@ -0,0 +1,8 @@
+#Subtração de dois números
+
+n1 = int(input("Digite o primeiro número: "))
+n2 = int(input("Digite o segundo número: "))
+
+subtracao = (n1 - n2)
+
+print("A subtração dos dois número é:", subtracao)
